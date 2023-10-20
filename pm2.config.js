8a4d3c74d376a431a7df749a31029ca9f6b03aa7
jjoek <jjohnirungu@gmail.com>
2023-10-20 14:11:25 +0300

    fix pm2 config file

--- pm2.config.js
+++ pm2.config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
       name: `${process.env.APP_NAME}-db-backup`,
       script: "./index.ts",
-      exec_mode: "ts-node",
+      interpreter: "./node_modules/.bin/ts-node",
       cron: process.env.BACKUP_CRON_SCHEDULE,
     },
   ],
